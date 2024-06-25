d01ae8110c807eb1031f5d747537584aa1765e6b
Chetan <chetanpl@gmail.com>
2024-06-25 12:38:00 +0100

    Fix: Automation

--- cypress/e2e/firstTest.cy.ts
+++ cypress/e2e/firstTest.cy.ts
@@ -1,17 +1,17 @@
 describe('template spec', () => {
-  it('Is Page title and first product available', () => {
+  it('Match the page title and color', () => {
     cy.visit('http://localhost:3000');
     cy.get('[data-testid="title"]').should("have.text","Choose Your Life Style");
     cy.get('[data-testid="productid"]').should('have.css','background-color').and('match',/255, 255, 255/);
     cy.get('[data-testid="productid"]').first('1');
   })
-  it('Search product name and match with reasult',()=>{
+  it('Automation auto enter product name and match with search result',()=>{
     cy.visit('http://localhost:3000')
     const productName = 'Foldsack';
     cy.typeWithDelay('[data-testid="seachInputCtrl"]', productName, 2);
     cy.get('[data-testid="productName"]').contains(productName);
   })
-  it('A Confirmation, Does pagination work',()=>{
+  it('Automation auto click page number and match it with active page',()=>{
     cy.visit('http://localhost:3000')
     const testid="pageNumber2";
     cy.get(`[data-testid=${testid}]`).click();
